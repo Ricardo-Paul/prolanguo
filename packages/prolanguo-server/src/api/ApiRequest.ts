9811d46a8b7a36a2a40e07483da1dd986f35bfc5
Ricardo-Paul <ricardopaul459@gmail.com>
2021-08-26 16:38:58 -0400

    chore: rename packages

---
+++ packages/prolanguo-server/src/api/ApiRequest.ts
@@ -0,0 +1,44 @@
+import { Request } from "@prolanguo/prolanguo-common/interfaces";
+import * as express from "express";
+
+// Resolver interface is a blueprint for resolver methods
+interface Resolver<T> {
+  resolve(arg: string): any
+}
+
+// abstractResolver defines the methods
+abstract class AbstractResolver<T extends object> implements Resolver<T> {
+  public resolve(val: string): T{
+    console.log("A value from resolve()", val)
+    const r: any = "";
+    return r;
+  }
+}
+
+//RequesResolver extends Abstract resolver (now has the resolve() method)
+export abstract class RequestResolver<T extends Request> extends AbstractResolver<Pick<T, 'query' | 'body'>>{}
+
+// SignUpResolver in between
+
+export class ApiRequest<T extends Request> {
+  private req: express.Request; //type for express
+  requestResolver: null | RequestResolver<T>
+
+  // TODO: add type annotation for requestResolver
+  // does requestResolver now has the props defined in abstractResolver?
+  constructor(req: express.Request, requestResolver: null | RequestResolver<T>){
+    this.req = req;
+    this.requestResolver = requestResolver;
+  }
+
+  public getResolver(): any{
+    if(this.requestResolver !== null){
+      console.log("Type of", typeof this.requestResolver)
+      console.log("Resolver object content :", this.requestResolver) //unefined
+      console.log("Resolving");
+      this.requestResolver.resolve("Hello World")
+      // this.requestResolver.resolve("Hello world")
+      // this.requestResolver.resolve("Hello world")
+    }
+  }
+}
