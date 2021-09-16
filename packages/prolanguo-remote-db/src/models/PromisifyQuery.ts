fa1ad79a2293854b24f5e9deac4db045f03d95a6
Ricardo-Paul <ricardopaul459@gmail.com>
2021-09-16 09:58:59 -0400

    feat: successfully insert user extra data into db

---
+++ packages/prolanguo-remote-db/src/models/PromisifyQuery.ts
@@ -0,0 +1,7 @@
+import { Knex } from "knex";
+
+export function promisifyQuery(query: Knex.QueryBuilder | Knex.Raw): Promise<any> {
+  return new Promise((resolve, reject): void=> {
+    query.then(resolve, reject)
+  })
+}
