01d496ab6c8b0a7c6c58f79d94d8425816b8e764
Ricardo-Paul <ricardopaul459@gmail.com>
2021-09-21 09:42:12 -0400

    refactor: move type declarations to common

--- packages/prolanguo-common/dist/interfaces/index.js
+++ packages/prolanguo-common/dist/interfaces/index.js
@@ -1,4 +1,5 @@
 "use strict";
 // exports each interface from the interface dir
+// from all folders inside interface
 Object.defineProperty(exports, "__esModule", { value: true });
 //# sourceMappingURL=index.js.map
--- packages/prolanguo-remote-db/src/interfaces/User.ts
+++ packages/prolanguo-remote-db/src/interfaces/User.ts
@@ -2,21 +2,6 @@
 import { UserMembership, UserStatus } from "@prolanguo/prolanguo-common/enums";
 import { UserExtraDataRow } from './UserExtraDataRow';
 
-// complete User representation in the program with extra data
-// without sensitive fileds (password, shardId, accessKey)
-export interface User {
-  readonly userId: string;
-  readonly email: string;
-  readonly userStatus: UserStatus;
-
-  readonly membership: UserMembership;
-  readonly membershipExpiredAt: null | Date;
-  readonly createdAt: Date;
-  readonly updatedAt: Date;
-  readonly firstSyncedAt: null | Date;
-  readonly lastSyncedAt: null | Date;
-  readonly extraData: UserExtraDataRow[] //add array type
-}
 
 // User fields as exists in the database
 export interface UserRow {
