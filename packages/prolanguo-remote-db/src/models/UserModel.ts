7c535bd48a4e4f8ad5ab6e32dc017f955e924035
Ricardo-Paul <ricardopaul459@gmail.com>
2021-10-06 15:52:00 -0400

    feat: get last updated time

--- packages/prolanguo-remote-db/src/models/UserModel.ts
+++ packages/prolanguo-remote-db/src/models/UserModel.ts
@@ -122,6 +122,35 @@ export class UserModel {
     )
   }
 
+  public async getLastUpdatedTime(db: Knex, userId: string): Promise<null | Date>{
+    return new Promise(
+      async (resolve, reject): Promise<void> => {
+        try{
+          const result = await promisifyQuery(
+            db.select('updatedAt')
+              .from(TableName.USER)
+              .where({ userId })
+              .limit(1)
+          );
+          const first = _.first(result);
+          if(typeof first === "undefined"){
+            resolve(null)
+          } else {
+            const userRow = this.userRowResolver.resolvePartial(first, true);
+            const { updatedAt } = userRow;
+            resolve(
+              assertExists(updatedAt,
+                "updatedAt must not be null or undefined"
+                )
+            );
+          }
+        }catch(error){
+          reject(error)
+        }
+      }
+    );
+  }
+
   public getUserById(db: Knex, userId: string): Promise<User | null> {
     return new Promise(
       async (resolve, reject): Promise<void> => {
