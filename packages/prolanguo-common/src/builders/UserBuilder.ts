69ae078afe0c746edf637a6044e07f9f1659c5a1
Ricardo-Paul <ricardopaul459@gmail.com>
2021-10-07 19:19:11 -0400

    feat: builder for user

--- packages/prolanguo-common/src/builders/UserBuilder.ts
+++ packages/prolanguo-common/src/builders/UserBuilder.ts
@@ -1,3 +1,35 @@
+import { UserExtraDataItemBuilder } from "./UserExtraDataItemBuilder";
+import { User } from "../interfaces/general/User";
+import * as _ from "lodash";
+import * as moment from "moment";
+import * as uuid from "uuid";
+import { UserMembership, UserStatus } from "../enums";
+import { UserExtraDataItem } from "./UserExtraDataItemBuilder"
+
 export class UserBuilder {
-  
+  public build(user: Partial<User>): User{
+    let extraData: UserExtraDataItem[] = [];
+    if(user.extraData !== undefined && user.extraData.length > 0){
+      extraData = user.extraData.map((extraDataItem): UserExtraDataItem => {
+        return new UserExtraDataItemBuilder().build(
+          extraDataItem
+        )
+      })
+    }
+
+    return _.merge({
+      userId: uuid.v4(),
+      email: "",
+      userStatus: UserStatus.ACTIVE,
+      membership: UserMembership.REGULAR,
+      membershipExpiredAt: null,
+      createdAt: moment.utc().toDate(),
+      updatedAt: moment.utc().toDate(),
+      firstSyncedAt: null,
+      lastSyncedAt: null,
+      extraData
+    },
+    user
+  )
+  }
 }
