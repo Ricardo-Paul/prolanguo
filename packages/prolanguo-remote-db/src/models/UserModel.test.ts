98002c7a465b3374834e8571867f2243297380f2
Ricardo-Paul <ricardopaul459@gmail.com>
2021-10-07 17:20:16 -0400

    test: test env loads properly and fix

--- packages/prolanguo-remote-db/src/models/UserModel.test.ts
+++ packages/prolanguo-remote-db/src/models/UserModel.test.ts
@@ -1,7 +1,39 @@
-describe('Test Model', () => {
-  // console.log("Test run")
-  it('it makes sure tests work', () => {
-    const num = 5;
-    expect(num).toBe(5);
+import { UserModel } from "./UserModel";
+import { DatabaseFacade } from "../facades/DatabaseFacade";
+import { Knex } from "knex";
+import { resolveEnv } from "../utils/resolveEnv";
+
+describe('Test UserModel', () => {
+  const env = resolveEnv();
+  test("Env var loads properly", () => {
+    expect(env).toEqual({
+      AUTH_DATABASE_CONFIG: expect.any(Object),
+      ALL_SHARD_DATABASE_CONFIG: expect.any(Object),
+      SHARD_DATABASE_PREFIX_NAME: expect.any(String)
+    })
+  })
+
+  describe('test starts with db connected', () => {
+    let databaseFacade: DatabaseFacade;
+    let authDb: Knex;
+    let userModel: UserModel;
+
+    beforeEach(async () => {
+      databaseFacade = new DatabaseFacade(
+        env.AUTH_DATABASE_CONFIG,
+        env.ALL_SHARD_DATABASE_CONFIG,
+        env.SHARD_DATABASE_PREFIX_NAME
+      );
+    });
+
+    afterEach(async () => {
+      // destroy all dbs here
+    });
+
+    test("inserts user succesfully ", () => {
+      expect(5).toEqual(5)
+    })
+    
   });
-})
+
+});
--- packages/prolanguo-remote-db/src/setupTest.ts
+++ packages/prolanguo-remote-db/src/setupTest.ts
@@ -1,2 +1,9 @@
+import * as dotenv from "dotenv";
 import 'jest-extended';
+import * as path from "path";
 jest.setTimeout(15000);
+
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "config", ".env")
+});
