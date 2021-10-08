141554bf15703bdb7ad9599f5f12ca189ae1f673
Ricardo-Paul <ricardopaul459@gmail.com>
2021-10-08 10:21:54 -0400

    feat: deep partial and assert exists

--- packages/prolanguo-common/src/builders/UserBuilder.ts
+++ packages/prolanguo-common/src/builders/UserBuilder.ts
@@ -4,17 +4,21 @@ import * as _ from "lodash";
 import * as moment from "moment";
 import * as uuid from "uuid";
 import { UserMembership, UserStatus } from "../enums";
-import { UserExtraDataItem } from "./UserExtraDataItemBuilder"
+import { UserExtraDataItem } from "../types/UserExtraDataItem";
+import { DeepPartial } from "../extended-types";
+import { assertExists } from "../assert/assertExists";
+
 
 export class UserBuilder {
-  public build(user: Partial<User>): User{
+  public build(user: DeepPartial<User>): User{
     let extraData: UserExtraDataItem[] = [];
-    if(user.extraData !== undefined && user.extraData.length > 0){
+
+    if(typeof user.extraData !== 'undefined'){
       extraData = user.extraData.map((extraDataItem): UserExtraDataItem => {
         return new UserExtraDataItemBuilder().build(
-          extraDataItem
-        )
-      })
+          assertExists(extraDataItem)
+        );
+      });
     }
 
     return _.merge({
@@ -31,5 +35,14 @@ export class UserBuilder {
     },
     user
   )
+  }
+}
+
+// testing deePartial Type
+const u = {name: "paul", occ: { job: "Dev", salary: 3000 }};
+const uPartial: DeepPartial<typeof u> = {
+  name: "eric",
+  occ: {
+    job: "Tester",
   }
 }
