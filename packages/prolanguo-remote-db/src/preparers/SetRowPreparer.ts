b07f4ad52626cb91e128cbd23689c1ded1fc24f4
Ricardo-Paul <ricardopaul459@gmail.com>
2021-10-09 14:13:17 -0400

    feat: prepare set row for insertion

---
+++ packages/prolanguo-remote-db/src/preparers/SetRowPreparer.ts
@@ -0,0 +1,55 @@
+import { AbstractPreparer } from "./AbstractPreparer";
+import * as Joi from "joi";
+import { SetStatus } from "@prolanguo/prolanguo-common/enums";
+import { Set } from "@prolanguo/prolanguo-common/interfaces";
+import * as _ from "lodash";
+import { SetRow } from "../interfaces/SetRow";
+import { SetRowForInsert } from "../interfaces/SetRow";
+
+
+export class SetRowPreparer extends AbstractPreparer<SetRow>{
+  protected insertRules = {
+    userId: Joi.string(),
+    setId: Joi.string(),
+    setName: Joi.string(),
+    setStatus: Joi.string().valid(..._.values(SetStatus)),
+    learningLanguageCode: Joi.string(),
+    translatedToLanguageCode: Joi.string(),
+    createdAt: Joi.date().optional(),
+    updatedAt: Joi.date().optional(),
+    updatedStatusAt: Joi.date(),
+    // may as well remove these lines
+    // leave them for compliance with SetRow interface
+    firstSyncedAt: Joi.forbidden().strip().optional(),
+    lastSyncedAt: Joi.forbidden().strip().optional(),
+  };
+
+  public prepareInsert(set: Set, userId: string){
+    const setRow = this.convertToInsertRow(set, userId);
+    return this.validateData(setRow, Joi.object(this.insertRules));
+  }
+
+  private convertToInsertRow(set: Set, userId: string): SetRowForInsert{
+    const {
+      setId, 
+      setName, 
+      setStatus, 
+      learningLanguageCode, 
+      translatedToLanguageCode,
+      createdAt,
+      updatedAt,
+      updatedStatusAt,
+    } = set
+    return {
+      userId,
+      setId,
+      setName,
+      setStatus,
+      learningLanguageCode,
+      translatedToLanguageCode,
+      createdAt,
+      updatedAt,
+      updatedStatusAt,
+    }
+  }
+}
