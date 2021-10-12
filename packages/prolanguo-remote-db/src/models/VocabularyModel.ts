7913497792635ed60d7928127e6096593b310970
Ricardo-Paul <ricardopaul459@gmail.com>
2021-10-12 08:51:09 -0400

    feat: vocabulary model

---
+++ packages/prolanguo-remote-db/src/models/VocabularyModel.ts
@@ -0,0 +1,162 @@
+import { Knex } from "knex";
+import { DeepPartial } from "@prolanguo/prolanguo-common/extended-types";
+import { TableName } from "../enums/tableName";
+import { promisifyQuery } from "./PromisifyQuery";
+import * as _ from "lodash";
+
+interface Vocabulary{
+
+}
+
+export class VocabularyModel{
+  public async upserMultipleVocabulary(
+    db: Knex,
+    userId: string,
+    vocabularySetIdPairs: [
+      DeepPartial<Vocabulary>, string
+    ][]
+  ): Promise<void>{
+    return new Promise(
+      async (resolve, reject): Promise<void> => {
+        const queries = [];
+
+        try{
+          // bulk insert vocabularies
+          if(vocabularySetIdPairs.length > 0){
+            queries.push(
+              this.insertOrIgnoreMultipleVocabulary(
+                db,
+                userId,
+                vocabularySetIdPairs.filter(
+                  ([Vocabulary, setId]) => {
+                    return this.vocabularyModelRowPreparer.canBeInserted(
+                      Vocabulary,
+                      setId,
+                      userId
+                    )
+                  }
+                )
+              )
+            );
+
+          // bulk update vocabularies
+          queries.push(
+            this.updateVocabularies(
+              db,
+              userId,
+              vocabularySetIdPairs
+            )
+          );
+        }
+        resolve()
+        }catch(error){
+          reject(error)
+        }
+      }
+    );
+  };
+
+  private async updateVocabularies(
+    db: Knex,
+    userId: string,
+    vocabularySetIdPairs: [
+      DeepPartial<Vocabulary>, string
+    ][]
+  ): Promise<void[]>{
+    return Promise.all(
+      vocabularySetIdPairs.map(
+        ([vocabulary, setId]) => {
+          return this.updateVocabulary(
+            db,
+            vocabulary,
+            setId,
+            userId
+          )
+        }
+      )
+    );
+  }
+
+  private async updateVocabulary(
+    db: Knex,
+    vocabulary: DeepPartial<Vocabulary>,
+    setId: string,
+    userId: string
+  ): Promise<void> {
+    return new Promise(
+      async (resolve, reject): Promise<void> => {
+        try{
+          const queries = [];
+
+          const vocabularyRows = this.vocabularyModelRowPreparer.prepareUpdate(
+            vocabulary,
+            setId,
+            userId
+          );
+      
+          const fieldsToUpdate = _.omit(vocabularyRows, ['vocabularyId, userId']);
+          const { vocabularyId } = vocabularyRows;
+      
+          queries.push(
+            await promisifyQuery(
+              db
+              .update(fieldsToUpdate)
+              .from(TableName.VOCABULARY)
+              .where({
+                vocabularyId,
+                userId
+              })
+            )
+          );
+        resolve()
+        }catch(error){
+          reject(error)
+        }
+      }
+    );
+  }
+
+  private async insertOrIgnoreMultipleVocabulary(
+    db: Knex,
+    userId: string,
+    vocabularySetIdPairs: [
+      DeepPartial<Vocabulary>, string
+    ][]
+  ): Promise<void>{
+    return new Promise(
+      async (resolve, reject): Promise<void> => {
+        try{
+          const queries = []
+          const vocabularyRows = vocabularySetIdPairs.map(
+            ([vocabulary, setId]) => {
+              return this.vocabularyModelRowPreparer.prepareInsert(
+                vocabulary,
+                setId,
+                userId
+              )
+            }
+          );
+
+          const { sql, bindings } = db
+            .insert(vocabularyRows)
+            .into(TableName.VOCABULARY)
+            .toSQL();
+
+          queries.push(
+            await promisifyQuery(
+              db.raw(
+                sql.replace('insert', 'insert ignore'), 
+                bindings
+              )
+            )
+          );
+
+        Promise.all(queries);
+        resolve()
+        }catch(error){
+          reject(error)
+        }
+      }
+    );
+  }
+}
