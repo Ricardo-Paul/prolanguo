53ece562bf939332cc68e730fd84e11c12172c2a
Ricardo-Paul <ricardopaul459@gmail.com>
2021-11-28 08:24:16 -0500

    feat: write update part of upsert for vocabulary

--- packages/prolanguo-remote-db/src/models/VocabularyCategoryModel.ts
+++ packages/prolanguo-remote-db/src/models/VocabularyCategoryModel.ts
@@ -4,6 +4,7 @@ import { DeepPartial } from "@prolanguo/prolanguo-common/extended-types";
 import { VocabularyCategoryRowPreparer } from "../preparers/VocabularyCategoryRowPreparer";
 import { TableName } from "../enums/tableName";
 import { promisifyQuery } from "./PromisifyQuery";
+import * as _ from "lodash";
 
 export class VocabularyCategoryModel{
   private vocabularyCategoryRowPreparer: VocabularyCategoryRowPreparer;
@@ -28,6 +29,7 @@ export class VocabularyCategoryModel{
             return this.vocabularyCategoryRowPreparer.prepareUpsert(category as Category, userId, vocabuaryId)
           });
 
+          // bulk insert vocabularies
           const { sql, bindings } = db
               .insert(categoryRows)
               .into(TableName.VOCABULARY_CATEGORY)
@@ -41,8 +43,26 @@ export class VocabularyCategoryModel{
             )
           );
 
-          console.log("Category Rows :", categoryRows);
-        
+          // bulk update vocabularies
+          queries.push(
+            ...categoryRows.map(async (row) => {
+              // remove fields we won't update
+              const fieldsToUpdate = _.omit(row, ['userId', 'vocabularyId']);
+              const { userId, vocabularyId } = row;
+              if(!_.isEmpty(fieldsToUpdate)){
+                await promisifyQuery(
+                  db
+                    .update(fieldsToUpdate)
+                    .from(TableName.VOCABULARY_CATEGORY)
+                    .where({
+                      userId,
+                      vocabularyId
+                    })
+                );
+              }
+            })
+          );
+
           await Promise.all(queries);
         resolve()
         }catch(error){
