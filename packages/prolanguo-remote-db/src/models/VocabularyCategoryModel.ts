1c5ae0812d6d41ccb052d04c373906f6bf013c55
Ricardo-Paul <ricardopaul459@gmail.com>
2021-11-27 22:03:34 -0500

    feat: inset category

--- packages/prolanguo-remote-db/src/models/VocabularyCategoryModel.ts
+++ packages/prolanguo-remote-db/src/models/VocabularyCategoryModel.ts
@@ -2,6 +2,8 @@ import { Knex } from "knex";
 import { Category } from "@prolanguo/prolanguo-common/interfaces";
 import { DeepPartial } from "@prolanguo/prolanguo-common/extended-types";
 import { VocabularyCategoryRowPreparer } from "../preparers/VocabularyCategoryRowPreparer";
+import { TableName } from "../enums/tableName";
+import { promisifyQuery } from "./PromisifyQuery";
 
 export class VocabularyCategoryModel{
   private vocabularyCategoryRowPreparer: VocabularyCategoryRowPreparer;
@@ -20,11 +22,28 @@ export class VocabularyCategoryModel{
     return new Promise(
       async (resolve, reject): Promise<void> => {
         try{
+          const queries = [];
+
           const categoryRows = categoryAndVocabularyIdPairs.map(([category, vocabuaryId]) => {
             return this.vocabularyCategoryRowPreparer.prepareUpsert(category as Category, userId, vocabuaryId)
           });
 
+          const { sql, bindings } = db
+              .insert(categoryRows)
+              .into(TableName.VOCABULARY_CATEGORY)
+              .toSQL();
+
+          queries.push(
+            promisifyQuery(
+              db.raw(
+                sql.replace('insert', 'insert ignore'), bindings
+              )
+            )
+          );
+
           console.log("Category Rows :", categoryRows);
+        
+          await Promise.all(queries);
         resolve()
         }catch(error){
           reject(error)
@@ -34,7 +53,7 @@ export class VocabularyCategoryModel{
   }
 
   public async getVocabularyCategoriesByIds(){
-    
+
   }
 
 }
