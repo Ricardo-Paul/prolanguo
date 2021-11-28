f32c286e9db910ccc2348e2454b8fc8d88711aee
Ricardo-Paul <ricardopaul459@gmail.com>
2021-11-28 09:33:23 -0500

    feat: bulk update vocabulary writings

--- packages/prolanguo-remote-db/src/interfaces/WritingRow.ts
+++ packages/prolanguo-remote-db/src/interfaces/WritingRow.ts
@@ -1,13 +1,15 @@
 import { DeepPartial } from "@prolanguo/prolanguo-common/extended-types";
 
-export interface CategoryRow {
+export interface WritinRow{
   readonly userId: string;
   readonly vocabularyId: string;
-  readonly categoryName: string;
+  readonly level: number;
+  readonly lastWritenAt: Date | null;
+  readonly disabled: boolean;
   readonly createdAt: Date | null;
   readonly updatedAt: Date | null;
   readonly firstSyncedAt: Date | null;
   readonly lastSyncedAt: Date | null;
 }
 
-export type CategoryRowForUpsert = DeepPartial<CategoryRow>;
+export type WritingRowForUpsert = DeepPartial<WritinRow>;
