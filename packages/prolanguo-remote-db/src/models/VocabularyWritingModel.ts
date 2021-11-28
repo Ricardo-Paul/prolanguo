b0502905c1db65b7f43ccd37306c8d3abc27a68b
Ricardo-Paul <ricardopaul459@gmail.com>
2021-11-28 09:33:23 -0500

    feat: bulk update vocabulary writings

--- packages/prolanguo-remote-db/src/models/VocabularyWritingModel.ts
+++ packages/prolanguo-remote-db/src/models/VocabularyWritingModel.ts
@@ -1,6 +1,78 @@
-export class VocabularyWritingModel{
-  public async upsertVocabularyWrings(){
+import { Knex } from "knex";
+import { VocabularyWriting } from "@prolanguo/prolanguo-common/interfaces";
+import { DeepPartial } from "@prolanguo/prolanguo-common/extended-types";
+import { VocabularyWritingRowPreparer } from "../preparers/VocabularyWritingRowPreparer";
+import { TableName } from "../enums/tableName";
+import { promisifyQuery } from "./PromisifyQuery";
+import * as _ from "lodash";
 
+export class VocabularyWritingModel{
+  private vocabularyWritingRowPreparer: VocabularyWritingRowPreparer;
+
+  constructor(){
+    this.vocabularyWritingRowPreparer = new VocabularyWritingRowPreparer();
+  }
+  public async upsertVocabularyWritings(
+    db: Knex,
+    userId: string,
+    writingAndVocabularyIdPairs: [
+      DeepPartial<VocabularyWriting>, string
+    ][]
+  ): Promise<void>{
+    return new Promise(
+      async (resolve, reject): Promise<void> => {
+        try{
+          const queries = [];
+
+          const vocabularyWritingRows = writingAndVocabularyIdPairs.map(
+            ([vocabularyWriting, vocabuaryId]) => {
+              return this.vocabularyWritingRowPreparer.prepareUpsert(
+                vocabularyWriting as VocabularyWriting,
+                userId,
+                vocabuaryId
+              )
+          });
+
+          // bulk insert vocabulary writings
+          const {sql, bindings} = db
+            .insert(vocabularyWritingRows)
+            .into(TableName.VOCABULARY_WRITING)
+            .toSQL();
+
+          queries.push(
+            await promisifyQuery(
+              db.raw(
+                sql.replace('insert', 'insert ignore'), bindings
+              )
+            )
+          );
+
+          // bulk update vocabulary writings
+          queries.push(
+            ...vocabularyWritingRows.map(async (row) => {
+              const fieldsToUpdate = _.omit(row, ['userId', 'vocabularyId']);
+              const { userId, vocabularyId } = row;
+              if(!_.isEmpty(fieldsToUpdate)){
+                await promisifyQuery(
+                  db
+                  .update(fieldsToUpdate)
+                  .from(TableName.VOCABULARY_WRITING)
+                  .where({
+                    userId,
+                    vocabularyId
+                  })
+                )
+              };
+            })
+          );
+
+        await Promise.all(queries);
+        resolve()
+        }catch(error){
+          reject(error)
+        }
+      }
+    );
   };
 
   public async getVocabularyWritingsByIds(){
