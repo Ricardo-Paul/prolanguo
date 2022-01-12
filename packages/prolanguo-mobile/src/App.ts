f42c2a951088ea977c3cebf4a10f52f4af900a83
Ricardo Paul <ricardo@Ricardos-MBP.lan>
2022-01-12 13:49:48 -0500

    feat: delegate to set root screen

--- packages/prolanguo-mobile/src/App.ts
+++ packages/prolanguo-mobile/src/App.ts
@@ -46,7 +46,6 @@ class App {
       }
     });
   }
-
 }
 
 export default App;
--- packages/prolanguo-mobile/src/views/welcome/WelcomeScreen.tsx
+++ packages/prolanguo-mobile/src/views/welcome/WelcomeScreen.tsx
@@ -4,7 +4,7 @@ import {Text } from "react-native";
 export class WelcomeScreen extends React.Component<{}>{
     public render(): React.ReactElement<any> {
         return (
-            <Text> Welcome to prolanguo </Text>
+            <Text> Prolanguo Home screen </Text>
         )
     }
 };
