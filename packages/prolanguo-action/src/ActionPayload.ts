bdd67eb2504df01dfed0523bd3dffe9e4ffddb05
Ricardo-Paul <ricardopaul459@gmail.com>
2022-01-14 12:54:23 -0500

    :feat: action helpers

---
+++ packages/prolanguo-action/src/ActionPayload.ts
@@ -0,0 +1,13 @@
+import { DeepPartial } from "@prolanguo/prolanguo-common/extended-types";
+import { ErroBag } from "@prolanguo/prolanguo-common/interfaces";
+
+// payload for each type of action
+export interface ActionPayload {
+    [P: string]: null | object;
+
+    readonly APP__ININTIALIZE: null;
+    readonly APP__INITIALIZING: null;
+    readonly APP__INITIALIZE_SUCCEEDED: null;
+    readonly APP__INITIALIZE_FAILED: ErroBag;
+    readonly APP__APP_ALREADY_INITIALED: null;
+}
