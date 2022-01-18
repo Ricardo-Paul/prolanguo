0d327dd8d5d7796778ea1c28b19f19df4772f7d0
Ricardo Paul <ricardo@Ricardos-MBP.lan>
2022-01-18 13:19:44 -0500

    allow event bus to execute disposer functions

--- packages/prolanguo-event/src/eventBus.ts
+++ packages/prolanguo-event/src/eventBus.ts
@@ -7,10 +7,12 @@ import { EventFacade } from "./eventFacade";
 export class EventBus{
     private store: Store;
     private eventFacade: EventFacade;
+    disposers: (() => void)[]
 
     constructor(store: Store, eventFacade: EventFacade){
         this.store = store;
         this.eventFacade = eventFacade;
+        this.disposers = []
     }
 
     public pubsub(action: Action<any>, listener: EventListener): () => void{
@@ -25,6 +27,15 @@ export class EventBus{
 
     subscribe(listener: EventListener): () => void{
         const disposer = this.eventFacade.subscribe(listener);
+        this.disposers.push(disposer);
         return disposer;
     }
+
+    usubscribeAll(){
+        this.disposers.forEach(
+            (dispoer) => {
+                dispoer();
+            }
+        )
+    };
 }
