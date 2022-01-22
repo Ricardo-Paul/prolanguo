b7f2e32f58c4f53724175e69e0b41f0e3a62abc9
Ricardo-Paul <ricardopaul459@gmail.com>
2022-01-22 12:03:25 -0500

    chore: drop react-native-navigation legacy method

--- packages/prolanguo-mobile/src/setup/registerScreens.tsx
+++ packages/prolanguo-mobile/src/setup/registerScreens.tsx
@@ -3,16 +3,17 @@ import * as _ from "lodash";
 import { extendContainer } from "../decorators/extendContainer";
 import { Navigation } from "react-native-navigation";
 import {Provider } from "../Provider";
+import React from "react";
 
 export function registerScreens(): void{
     _.forOwn(
         ScreenContainers,
         (container, screenName): void => {
-            Navigation.registerComponentWithRedux(
-                screenName,
-                () => extendContainer(container),
-                Provider,
-                null
+            const Child = extendContainer(container);
+            Navigation.registerComponent(screenName, () =>  (props) => 
+                <Provider>
+                    <Child {...props} />
+                </Provider>
             )
         }
     )
